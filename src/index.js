8688e57051387831e6adb85d9adca6e5933102e8
maxbendas <56072735+maxbendas@users.noreply.github.com>
2020-10-02 23:13:37 +0300

    6-7

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log('fff')
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
+
+ReactDOM.render(<App />,
+    document.getElementById('root'));
