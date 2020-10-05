36bcbb83410c440a40eaa1873b8a1f388b09e5e0
maxbendas <maxbendas86@gmail.com>
2020-10-05 15:53:08 +0300

    7-8

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,23 +6,39 @@ import ItemList from '../item-list';
 import PersonDetails from '../person-details';
 
 import './app.css';
+import ErrorButton from "../error-button";
+import ErrorIndicator from "../error-indicator";
 
 class App extends Component {
-    state={
-        showRandomPlanet: true
+    state = {
+        showRandomPlanet: true,
+        selectedPerson: 5,
+        hasError: false
     }
 
-    toggleRandomPlanet = () =>{
-        this.setState((state)=>{
+    toggleRandomPlanet = () => {
+        this.setState((state) => {
             return {
-                showRandomPlanet : !state.showRandomPlanet
+                showRandomPlanet: !state.showRandomPlanet
             }
         })
     }
 
+    onPersonSelected = (id) => {
+        this.setState({
+            selectedPerson: id
+        })
+    }
+componentDidCatch(error, errorInfo) {
+        this.setState({hasError:true})
+}
+
     render() {
+        if (this.state.hasError){
+            return <ErrorIndicator/>
+        }
         const {showRandomPlanet} = this.state
-        const planet = showRandomPlanet ? <RandomPlanet/>:null
+        const planet = showRandomPlanet ? <RandomPlanet/> : null
 
         return (
             <div>
@@ -33,13 +49,13 @@ class App extends Component {
                     onClick={this.toggleRandomPlanet}>
                     Toggle Random Planet
                 </button>
-
+<ErrorButton/>
                 <div className="row mb2">
                     <div className="col-md-6">
-                        <ItemList/>
+                        <ItemList onItemSelected={this.onPersonSelected}/>
                     </div>
                     <div className="col-md-6">
-                        <PersonDetails/>
+                        <PersonDetails personId={this.state.selectedPerson}/>
                     </div>
                 </div>
             </div>
